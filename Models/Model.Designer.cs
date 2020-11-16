addd2a1d86ace1eefa0b2c8ac52707ad076794cd
juanjocpalencia <43501446+juanjocpalencia@users.noreply.github.com>
2020-11-16 11:53:56 -0600

    cambios

--- Models/Model.Designer.cs
+++ Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\JuanJo\source\repos\E-comerce\E-comerce\Models\Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\JuanJo\Desktop\Github-loar\comercio\E-comerce\Models\Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
